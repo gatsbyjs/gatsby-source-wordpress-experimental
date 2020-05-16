88a4a059741acc2527b51965640241e870cd2144
Tyler Barnes <tylerdbarnes@gmail.com>
2020-05-15 17:53:34 -0700

    move plugin options validation and processing to it's own function & config object

---
+++ src/steps/process-and-validate-plugin-options.js
@@ -0,0 +1,65 @@
+import store from "~/store"
+import { formatLogMessage } from "~/utils/format-log-message"
+import isInteger from "lodash/isInteger"
+
+const optionsProcessors = [
+  {
+    name: `excludeFields-renamed-to-excludeFieldNames`,
+    test: ({ pluginOptions }) => pluginOptions?.excludeFields?.length,
+    processor: ({ helpers, pluginOptions }) => {
+      helpers.reporter.log(``)
+      helpers.reporter.warn(
+        formatLogMessage(
+          // @todo
+          `\n\nPlugin options excludeFields has been renamed to excludeFieldNames.\nBoth options work for now, but excludeFields will be removed in a future version\n(likely when we get to beta) in favour of excludeFieldNames.\n\n`
+        )
+      )
+
+      if (
+        pluginOptions?.excludeFieldNames?.length ||
+        // @todo remove excludeFields option in beta release since it's been renamed to excludeFieldNames
+        pluginOptions?.excludeFields?.length
+      ) {
+        store.dispatch.remoteSchema.addFieldsToBlackList(
+          pluginOptions.excludeFieldNames || pluginOptions.excludeFields
+        )
+      }
+    },
+  },
+  {
+    name: `queryDepth-is-not-a-positive-int`,
+    test: ({ pluginOptions }) =>
+      !isInteger(pluginOptions.schema.queryDepth) ||
+      pluginOptions.schema.queryDepth <= 0,
+    processor: ({ helpers, pluginOptions }) => {
+      helpers.reporter.log(``)
+      helpers.reporter.warn(
+        formatLogMessage(
+          `\n\npluginOptions.schema.queryDepth is not a positive integer.\nUsing default value in place of provided value.\n`,
+          { useVerboseStyle: true }
+        )
+      )
+
+      delete pluginOptions.schema.queryDepth
+
+      return pluginOptions
+    },
+  },
+]
+
+export const processAndValidatePluginOptions = (helpers, pluginOptions) => {
+  let pluginOptionsCopy = {
+    ...pluginOptions,
+  }
+
+  optionsProcessors.forEach(({ test, processor }) => {
+    if (test({ helpers, pluginOptions: pluginOptionsCopy })) {
+      pluginOptionsCopy = processor({
+        helpers,
+        pluginOptions: pluginOptionsCopy,
+      })
+    }
+  })
+
+  return pluginOptionsCopy
+}
--- src/steps/set-gatsby-api-to-state.js
+++ src/steps/set-gatsby-api-to-state.js
@@ -1,42 +1,23 @@
 import store from "~/store"
-import { formatLogMessage } from "~/utils/format-log-message"
+import { processAndValidatePluginOptions } from "./process-and-validate-plugin-options"
 
 const setGatsbyApiToState = (helpers, pluginOptions) => {
   if (helpers.traceId === `refresh-createSchemaCustomization`) {
     return
   }
 
+  const filteredPluginOptions = processAndValidatePluginOptions(
+    helpers,
+    pluginOptions
+  )
+
   //
   // add the plugin options and Gatsby API helpers to our store
   // to access them more easily
   store.dispatch.gatsbyApi.setState({
     helpers,
-    pluginOptions,
+    pluginOptions: filteredPluginOptions,
   })
-
-  if (!pluginOptions) {
-    return
-  }
-
-  if (pluginOptions?.excludeFields?.length) {
-    helpers.reporter.log(``)
-    helpers.reporter.warn(
-      formatLogMessage(
-        // @todo
-        `\n\nPlugin options excludeFields has been renamed to excludeFieldNames.\nBoth options work for now, but excludeFields will be removed in a future version\n(likely when we get to beta) in favour of excludeFieldNames.\n\n`
-      )
-    )
-  }
-
-  if (
-    pluginOptions?.excludeFieldNames?.length ||
-    // @todo remove excludeFields option in beta release since it's been renamed to excludeFieldNames
-    pluginOptions?.excludeFields?.length
-  ) {
-    store.dispatch.remoteSchema.addFieldsToBlackList(
-      pluginOptions.excludeFieldNames || pluginOptions.excludeFields
-    )
-  }
 }
 
 export { setGatsbyApiToState }
