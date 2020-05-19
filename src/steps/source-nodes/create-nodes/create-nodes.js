ce6d5b6c3c3e4ccb1234434a0dc24ffa5935f90d
Tyler Barnes <tylerdbarnes@gmail.com>
2020-05-19 11:45:09 -0700

    add node processing to search for img tags

--- src/steps/source-nodes/create-nodes/create-nodes.js
+++ src/steps/source-nodes/create-nodes/create-nodes.js
@@ -1,5 +1,3 @@
-import stringify from "fast-json-stable-stringify"
-import execall from "execall"
 import PQueue from "p-queue"
 import fetchReferencedMediaItemsAndCreateNodes from "../fetch-nodes/fetch-referenced-media-items"
 import urlToPath from "~/utils/url-to-path"
@@ -11,6 +9,7 @@ import {
   buildTypeName,
   getTypeSettingsByType,
 } from "~/steps/create-schema-customization/helpers"
+import { processNode } from "./process-node"
 
 // @todo concurrency is currently set so low because side effects can overwhelm
 // the remote server. A queue for the entire source plugin should be created so that
@@ -18,8 +17,6 @@ import {
 const createNodesQueue = new PQueue({
   concurrency: 2,
 })
-
-// const imgSrcRemoteFileRegex = /(?:src=\\")((?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])\.(?:jpeg|jpg|png|gif|ico|pdf|doc|docx|ppt|pptx|pps|ppsx|odt|xls|psd|mp3|m4a|ogg|wav|mp4|m4v|mov|wmv|avi|mpg|ogv|3gp|3g2|svg|bmp|tif|tiff|asf|asx|wm|wmx|divx|flv|qt|mpe|webm|mkv|txt|asc|c|cc|h|csv|tsv|ics|rtx|css|htm|html|m4b|ra|ram|mid|midi|wax|mka|rtf|js|swf|class|tar|zip|gz|gzip|rar|7z|exe|pot|wri|xla|xlt|xlw|mdb|mpp|docm|dotx|dotm|xlsm|xlsb|xltx|xltm|xlam|pptm|ppsm|potx|potm|ppam|sldx|sldm|onetoc|onetoc2|onetmp|onepkg|odp|ods|odg|odc|odb|odf|wp|wpd|key|numbers|pages))(?=\\"| |\.)/gim
 
 const createNodeWithSideEffects = ({
   node,
@@ -32,6 +29,7 @@ const createNodeWithSideEffects = ({
   createdNodeIds,
   createNodesActivity,
   totalSideEffectNodes,
+  wpUrl,
 }) => async () => {
   if (node.link) {
     // create a pathname for the node using the WP permalink
@@ -44,24 +42,12 @@ const createNodeWithSideEffects = ({
   // this is important not only for downloading only used images
   // but also for downloading images in post content
   if (wpgqlNodesGroup.plural !== `mediaItems`) {
-    const nodeString = stringify(node)
-
-    // const imageUrlMatches = execall(imgSrcRemoteFileRegex, nodeString)
-
-    // if (imageUrlMatches.length) {
-    //   store.dispatch.imageNodes.addImgMatches(imageUrlMatches)
-    // }
-
-    if (!pluginOptions.type.MediaItem.lazyNodes) {
-      // get an array of all referenced media file ID's
-      const matchedIds = execall(/"id":"([^"]*)","sourceUrl"/gm, nodeString)
-        .map((match) => match.subMatches[0])
-        .filter((id) => id !== node.id)
-
-      if (matchedIds.length) {
-        matchedIds.forEach((id) => referencedMediaItemNodeIds.add(id))
-      }
-    }
+    node = processNode({
+      node,
+      pluginOptions,
+      referencedMediaItemNodeIds,
+      wpUrl,
+    })
   }
 
   const remoteNode = {
@@ -130,12 +116,6 @@ export const createGatsbyNodesFromWPGQLContentNodes = async ({
     `,
   })
 
-  const anchorTagRegex = new RegExp(
-    // eslint-disable-next-line no-useless-escape
-    `<a[\\\s]+[^>]*?href[\\\s]?=["'\\\\]*(${wpUrl}.*?)["'\\\\]*.*?>([^<]+|.*?)?<\/a>`,
-    `gim`
-  )
-
   // wp supports these file extensions
   // jpeg|jpg|png|gif|ico|pdf|doc|docx|ppt|pptx|pps|ppsx|odt|xls|psd|mp3|m4a|ogg|wav|mp4|m4v|mov|wmv|avi|mpg|ogv|3gp|3g2|svg|bmp|tif|tiff|asf|asx|wm|wmx|divx|flv|qt|mpe|webm|mkv|txt|asc|c|cc|h|csv|tsv|ics|rtx|css|htm|html|m4b|ra|ram|mid|midi|wax|mka|rtf|js|swf|class|tar|zip|gz|gzip|rar|7z|exe|pot|wri|xla|xlt|xlw|mdb|mpp|docm|dotx|dotm|xlsm|xlsb|xltx|xltm|xlam|pptm|ppsm|potx|potm|ppam|sldx|sldm|onetoc|onetoc2|onetmp|onepkg|odp|ods|odg|odc|odb|odf|wp|wpd|key|numbers|pages
 
@@ -164,6 +144,7 @@ export const createGatsbyNodesFromWPGQLContentNodes = async ({
           createdNodeIds,
           createNodesActivity,
           totalSideEffectNodes,
+          wpUrl,
         })
       )
     }
