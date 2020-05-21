0b492da4fd986ff34f454686b203a74dafe08009
Tyler Barnes <tylerdbarnes@gmail.com>
2020-05-21 13:15:22 -0700

    if getting img by src url doesn't work, try by id

--- src/steps/source-nodes/create-nodes/process-node.js
+++ src/steps/source-nodes/create-nodes/process-node.js
@@ -9,8 +9,10 @@ import fetchReferencedMediaItemsAndCreateNodes, {
   stripImageSizesFromUrl,
 } from "../fetch-nodes/fetch-referenced-media-items"
 import store from "~/store"
+import btoa from "btoa"
 
-const imgSrcRemoteFileRegex = /(?:src=\\")((?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])\.(?:jpeg|jpg|png|gif|ico|pdf|doc|docx|ppt|pptx|pps|ppsx|odt|xls|psd|mp3|m4a|ogg|wav|mp4|m4v|mov|wmv|avi|mpg|ogv|3gp|3g2|svg|bmp|tif|tiff|asf|asx|wm|wmx|divx|flv|qt|mpe|webm|mkv|txt|asc|c|cc|h|csv|tsv|ics|rtx|css|htm|html|m4b|ra|ram|mid|midi|wax|mka|rtf|js|swf|class|tar|zip|gz|gzip|rar|7z|exe|pot|wri|xla|xlt|xlw|mdb|mpp|docm|dotx|dotm|xlsm|xlsb|xltx|xltm|xlam|pptm|ppsm|potx|potm|ppam|sldx|sldm|onetoc|onetoc2|onetmp|onepkg|odp|ods|odg|odc|odb|odf|wp|wpd|key|numbers|pages))(?=\\"| |\.)/gim
+// @todo this doesn't make sense because these aren't all images
+const imgSrcRemoteFileRegex = /(?:src=\\")((?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])\.(?:xjpeg|jpg|png|gif|ico|pdf|doc|docx|ppt|pptx|pps|ppsx|odt|xls|psd|mp3|m4a|ogg|wav|mp4|m4v|mov|wmv|avi|mpg|ogv|3gp|3g2|svg|bmp|tif|tiff|asf|asx|wm|wmx|divx|flv|qt|mpe|webm|mkv|tt|asc|c|cc|h|csv|tsv|ics|rtx|css|htm|html|m4b|ra|ram|mid|midi|wax|mka|rtf|js|swf|class|tar|zip|gz|gzip|rar|7z|exe|pot|wri|xla|xlt|xlw|mdb|mpp|docm|dotx|dotm|xlsm|xlsb|xltx|xltm|xlam|pptm|ppsm|potx|potm|ppam|sldx|sldm|onetoc|onetoc2|onetmp|onepkg|odp|ods|odg|odc|odb|odf|wp|wpd|key|numbers|pages))(?=\\"| |\.)/gim
 
 const imgTagRegex = /<img([\w\W]+?)[\/]?>/gim
 
@@ -34,6 +36,13 @@ const findReferencedImageNodeIds = ({
   return matchedIds
 }
 
+const getCheerioImgDataId = (cheerioImg) => {
+  if (!cheerioImg.attribs) {
+    dd(cheerioImg)
+  }
+  return cheerioImg.attribs[`data-id`] || cheerioImg.attribs[`data-image-id`]
+}
+
 const fetchNodeHtmlImageMediaItemNodes = async ({ cheerioImages }) => {
   // check if we have any of these nodes locally already
   // build a query to fetch all media items that we don't already have
@@ -41,25 +50,62 @@ const fetchNodeHtmlImageMediaItemNodes = async ({ cheerioImages }) => {
     ({ cheerioImg }) => cheerioImg.attribs.src
   )
 
-  const mediaItemNodes = await fetchReferencedMediaItemsAndCreateNodes({
-    mediaItemUrls,
+  const mediaItemNodesBySourceUrl = await fetchReferencedMediaItemsAndCreateNodes(
+    {
+      mediaItemUrls,
+    }
+  )
+
+  // images that have been edited from the media library that were previously
+  // uploaded to a post/page will have a different sourceUrl so they can't be fetched by it
+  // in many cases we have data-id or data-image-id as attributes on the img
+  // we can try to use those to fetch media item nodes as well
+  // this will keep us from missing nodes
+  const mediaItemDbIds = cheerioImages
+    .map(({ cheerioImg }) => getCheerioImgDataId(cheerioImg))
+    .filter(Boolean)
+
+  // media items are of the post type
+  const mediaItemRelayIds = mediaItemDbIds
+    .map((dbId) => btoa(`post:${dbId}`))
+    // filter out any media item ids we already fetched
+    .filter(
+      (relayId) => !mediaItemNodesBySourceUrl.find((id) => id !== relayId)
+    )
+
+  const mediaItemNodesById = await fetchReferencedMediaItemsAndCreateNodes({
+    referencedMediaItemNodeIds: mediaItemRelayIds,
   })
 
-  const htmlMatchesToMediaItemNodesMap = mediaItemNodes.reduce(
-    (accumulator, mediaItemNode) => {
-      const { match } =
-        cheerioImages.find(({ cheerioImg }) =>
-          [
-            // try to match the media item source url by original html src
-            cheerioImg.attribs.src,
-            // or by the src minus any image sizes string
-            stripImageSizesFromUrl(cheerioImg.attribs.src),
-          ].includes(mediaItemNode.sourceUrl)
-        ) || {}
+  const mediaItemNodes = [...mediaItemNodesById, ...mediaItemNodesBySourceUrl]
 
-      if (!match) {
+  if (mediaItemRelayIds.length) {
+    dump(mediaItemNodesById.map((node) => node.id))
+    dd(mediaItemNodesBySourceUrl.map((node) => node.id))
+  }
+
+  const htmlMatchesToMediaItemNodesMap = cheerioImages.reduce(
+    (accumulator, { cheerioImg, match }) => {
+      const possibleHtmlSrcs = [
+        // try to match the media item source url by original html src
+        cheerioImg.attribs.src,
+        // or by the src minus any image sizes string
+        stripImageSizesFromUrl(cheerioImg.attribs.src),
+      ]
+
+      const mediaItemNode = mediaItemNodes.find(
+        (mediaItemNode) =>
+          possibleHtmlSrcs.includes(mediaItemNode.sourceUrl) ||
+          getCheerioImgDataId(cheerioImg) === mediaItemNode.id
+      )
+
+      if (!mediaItemNode) {
         dump(`no html image found for node:`)
-        dd(mediaItemNode)
+        dump(match)
+        dump(possibleHtmlSrcs)
+        dump(mediaItemUrls)
+        dump(cheerioImg)
+        dd(mediaItemNodes.map(({ sourceUrl }) => sourceUrl))
       }
 
       // match is the html string of the img tag
@@ -122,6 +168,10 @@ const replaceNodeHtmlImages = async ({
     const htmlMatchesToGatsbyImgStringMap = await Promise.all(
       imgTagMatches.map(async ({ match }) => {
         const mediaItemNode = htmlMatchesToMediaItemNodesMap.get(match)
+        if (!mediaItemNode) {
+          dd(Array.from(htmlMatchesToMediaItemNodesMap.entries()))
+        }
+
         const fileNode = helpers.getNode(mediaItemNode.remoteFile.id)
 
         dump(`fluid result coming up`)
