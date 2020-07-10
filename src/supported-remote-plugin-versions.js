ef398ca1be8bf4bf3ada9db85074f1d17956bc60
Tyler Barnes <tylerdbarnes@gmail.com>
2020-07-10 07:42:01 -0700

    fix previews for drafts and new post drafts

--- src/supported-remote-plugin-versions.js
+++ src/supported-remote-plugin-versions.js
@@ -6,8 +6,8 @@ const supportedWpPluginVersions = {
     reason: `WPGraphQL 0.10.0 introduced a large number of changes that required internal source plugin code changes. A big feature is improved support for WordPress previews and improvements to how Menus are handled. Head to https://github.com/wp-graphql/wp-graphql/releases/tag/v0.10.0 for more information on upgrading.`,
   },
   WPGatsby: {
-    version: `~0.4.0`,
-    reason: `WPGatsby 0.4.0 supports WPGraphQL 0.10.0`,
+    version: `~0.4.1`,
+    reason: `WPGatsby 0.4.1 supports WPGraphQL 0.10.0`,
   },
 }
 
