76f0a003ff76cf95ae5432dbb338581449fcc9d5
Tyler Barnes <tylerdbarnes@gmail.com>
2020-07-22 23:15:03 -0700

    process root field node to catch and fetch missing referenced media item nodes

--- src/steps/source-nodes/create-nodes/fetch-and-create-non-node-root-fields.js
+++ src/steps/source-nodes/create-nodes/fetch-and-create-non-node-root-fields.js
@@ -1,17 +1,19 @@
 import store from "~/store"
 import fetchGraphql from "~/utils/fetch-graphql"
-import { createContentDigest } from "gatsby-core-utils"
 import { formatLogMessage } from "~/utils/format-log-message"
+import { createNodeWithSideEffects } from "./create-nodes"
+import fetchReferencedMediaItemsAndCreateNodes from "../fetch-nodes/fetch-referenced-media-items"
+import { CREATED_NODE_IDS } from "~/constants"
 
 const fetchAndCreateNonNodeRootFields = async () => {
   const {
-    remoteSchema: { nonNodeQuery },
+    remoteSchema: { nonNodeQuery, wpUrl },
     gatsbyApi: { helpers, pluginOptions },
   } = store.getState()
 
-  const activity = helpers.reporter.activityTimer(
-    formatLogMessage(`fetch root fields`)
-  )
+  const { actions, createContentDigest, cache, reporter } = helpers
+
+  const activity = reporter.activityTimer(formatLogMessage(`fetch root fields`))
 
   activity.start()
 
@@ -20,15 +22,71 @@ const fetchAndCreateNonNodeRootFields = async () => {
     errorContext: `Error occured while fetching non-Node root fields.`,
   })
 
-  await helpers.actions.createNode({
+  const createdNodeIds = []
+  // const totalSideEffectNodes = []
+  const referencedMediaItemNodeIds = new Set()
+
+  const type = pluginOptions.schema.typePrefix
+
+  const node = {
     ...data,
     id: `${pluginOptions.url}--rootfields`,
-    parent: null,
-    internal: {
-      type: pluginOptions.schema.typePrefix,
-      contentDigest: createContentDigest(data),
-    },
+    type,
+  }
+
+  const createRootNode = createNodeWithSideEffects({
+    node,
+    actions,
+    createContentDigest,
+    pluginOptions,
+    referencedMediaItemNodeIds,
+    helpers,
+    createdNodeIds,
+    // totalSideEffectNodes,
+    wpUrl,
+    type,
   })
+
+  await createRootNode()
+
+  const referencedMediaItemNodeIdsArray = [...referencedMediaItemNodeIds]
+
+  const newMediaItemIds = referencedMediaItemNodeIdsArray.filter(
+    (id) => !helpers.getNode(id)
+  )
+
+  /**
+   * if we're not lazy fetching media items, we need to fetch them
+   * upfront here
+   */
+  if (!pluginOptions.type.MediaItem.lazyNodes && newMediaItemIds.length) {
+    store.dispatch.logger.createActivityTimer({
+      typeName: `MediaItems`,
+      pluginOptions,
+      reporter,
+    })
+
+    await fetchReferencedMediaItemsAndCreateNodes({
+      referencedMediaItemNodeIds: newMediaItemIds,
+    })
+
+    const previouslyCachedNodeIds = await cache.get(CREATED_NODE_IDS)
+
+    const createdNodeIds = [
+      ...new Set([
+        ...previouslyCachedNodeIds,
+        ...referencedMediaItemNodeIdsArray,
+      ]),
+    ]
+
+    // save the node id's so we can touch them on the next build
+    // so that we don't have to refetch all nodes
+    await cache.set(CREATED_NODE_IDS, createdNodeIds)
+
+    store.dispatch.logger.stopActivityTimer({
+      typeName: `MediaItems`,
+    })
+  }
 
   activity.end()
 }
