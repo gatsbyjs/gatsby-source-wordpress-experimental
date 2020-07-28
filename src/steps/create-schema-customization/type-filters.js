cd18cb89a89a0faae21cafc4c27edd3ac6f8c78b
Tyler Barnes <tylerdbarnes@gmail.com>
2020-07-27 18:17:07 -0700

    add more Date resolvers

--- src/steps/create-schema-customization/type-filters.js
+++ src/steps/create-schema-customization/type-filters.js
@@ -5,6 +5,11 @@ export const typeDefinitionFilters = [
   {
     typeName: `__all`,
     typeDef: (typeDef) => {
+      /**
+       * @todo once WPGraphQL has a DateTime Scalar, use that to find date fields
+       * instead of the below fieldnames
+       */
+
       if (typeDef?.fields?.date) {
         const dateField = {
           ...typeDef.fields.date,
@@ -15,6 +20,42 @@ export const typeDefinitionFilters = [
         }
 
         typeDef.fields.date = dateField
+      }
+
+      if (typeDef?.fields?.dateGmt) {
+        const dateField = {
+          ...typeDef.fields.dateGmt,
+          type: `Date`,
+          extensions: {
+            dateformat: {},
+          },
+        }
+
+        typeDef.fields.dateGmt = dateField
+      }
+
+      if (typeDef?.fields?.modified) {
+        const dateField = {
+          ...typeDef.fields.modified,
+          type: `Date`,
+          extensions: {
+            dateformat: {},
+          },
+        }
+
+        typeDef.fields.modified = dateField
+      }
+
+      if (typeDef?.fields?.modifiedGmt) {
+        const dateField = {
+          ...typeDef.fields.modifiedGmt,
+          type: `Date`,
+          extensions: {
+            dateformat: {},
+          },
+        }
+
+        typeDef.fields.modifiedGmt = dateField
       }
 
       return typeDef
