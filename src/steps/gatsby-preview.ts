dba8861d1aac2f15634119e03cdcf9e23ebc65dd
Tyler Barnes <tylerdbarnes@gmail.com>
2020-10-20 18:38:32 -0700

    keep it that pageContext.id is required

--- src/steps/gatsby-preview.ts
+++ src/steps/gatsby-preview.ts
@@ -24,29 +24,44 @@ const listenToWebsocket = ({ getNode }): void => {
         console.log(`subscribing to ${nodeId}`)
         const existingNode = getNode(nodeId)
 
-        function wasNodeUpdated({ node: possiblyUpdatedNode, modifiedDate }) {
-          console.log({
-            possiblyUpdatedModifiedTime: possiblyUpdatedNode.modified,
-            websocketSentModifiedTime: modifiedDate,
-          })
-          return (
+        function wasNodeUpdated({
+          node: possiblyUpdatedNode,
+          modifiedDate,
+          context,
+        }) {
+          const nodeWasUpdated = !!(
             possiblyUpdatedNode &&
             // if the modifiedDate is after or equal to the modified date of the node
             // then the node was updated already
             new Date(possiblyUpdatedNode.modified).getTime() >=
               new Date(modifiedDate).getTime()
           )
+
+          console.log({
+            possiblyUpdatedModifiedTime: possiblyUpdatedNode.modified,
+            websocketSentModifiedTime: modifiedDate,
+            nodeWasUpdated,
+            context,
+          })
+
+          return nodeWasUpdated
         }
 
         function onPageCreatedCallback({ passedNode, pageNode, context }) {
-          if (!wasNodeUpdated({ node: passedNode, modifiedDate: modified })) {
+          if (
+            !wasNodeUpdated({
+              node: passedNode,
+              modifiedDate: modified,
+              context,
+            })
+          ) {
             console.log(
-              `node was not updated yet but onPageCreatedCallback was called`
+              `node was not updated yet but onPageCreatedCallback was called ${context}`
             )
-            return
+            return false
           }
 
-          if (pageNode && pageNode.updatedAt < Date.now()) {
+          if (pageNode && pageNode.updatedAt <= Date.now()) {
             // this node was already updated, no need to subcribe
             // just send the node back
             console.log(`sending back to ${passedNode.id} from ${context}`)
@@ -56,21 +71,36 @@ const listenToWebsocket = ({ getNode }): void => {
             store.dispatch.previewStore.unSubscribeToPagesCreatedFromNodeById({
               nodeId: passedNode.id,
             })
-            return
+            return true
+          } else {
+            console.log(`node was updated but no pageNode?`)
+            console.log(pageNode)
+            return false
           }
         }
 
-        const { page } =
-          store.getState().previewStore.nodePageCreatedStateByNodeId[nodeId] ??
-          {}
+        const nodePagesCreatedByNodeIds = store.getState().previewStore
+          .nodePageCreatedStateByNodeId
+
+        const thisNodePage = nodePagesCreatedByNodeIds?.[nodeId]
+
+        const { page } = thisNodePage ?? {}
 
         if (existingNode) {
           // call the callback immediately. It's internal checks will determine wether it's the right time or not.
-          onPageCreatedCallback({
+          const wasPreviewLoaded = onPageCreatedCallback({
             passedNode: existingNode,
             pageNode: page,
             context: `existing node`,
           })
+
+          if (wasPreviewLoaded) {
+            console.log(`existing node preview was loaded`)
+            // return
+          } else {
+            console.log(`existing node preview was not loaded`)
+            nodePagesCreatedByNodeIds
+          }
         }
 
         if (!inPreviewMode) {
@@ -87,6 +117,7 @@ const listenToWebsocket = ({ getNode }): void => {
         }
 
         console.log(`setting up subscriber`)
+
         // if this node & page haven't been updated yet, set up a subscriber callback
         store.dispatch.previewStore.subscribeToPagesCreatedFromNodeById({
           nodeId,
@@ -125,31 +156,45 @@ export const savePreviewNodeIdToPageDependency = (helpers) => {
 
   let nodeThatCreatedThisPage = contextNode
 
-  // we want to try to get the node by context id
-  // because otherwise we need to look it up expensively in componentDataDependencies
-  // by finding the map key (nodeId) where the map value is an array containing page.path 😱 not good
-  if (!nodeThatCreatedThisPage) {
-    const state = gatsbyState.getState()
-
-    const getMapKeyByValue = (val) => {
-      /**
-       * this is expensive because we're looking up a map key (node id) by wether our value exists within it's value which is an array of page paths
-       */
-      const returnedEntries = [
-        ...state.componentDataDependencies.nodes,
-      ].find(([, value]) => [...value].includes(val))
-
-      if (returnedEntries && returnedEntries.length) {
-        return returnedEntries[0]
-      }
-
-      return null
-    }
-
-    const nodeId = getMapKeyByValue(page.path)
-    // console.log(`state node id ${nodeId}`)
-    nodeThatCreatedThisPage = getNode(nodeId)
+  if (!contextNode) {
+    return
   }
+
+  // the following code could be used to remove the requirement that
+  // node.id is passed to pageContext when creating pages.
+  // Unfortunately it was really buggy in the current implementation
+  // So folks can just add the id to pageContext and it will work
+  // Leaving this in case someone comes here trying to remove the pageContext.id
+  // requirement in the future
+  //
+  // // we want to try to get the node by context id
+  // // because otherwise we need to look it up expensively in componentDataDependencies
+  // // by finding the map key (nodeId) where the map value is an array containing page.path 😱 not good
+  // if (!nodeThatCreatedThisPage) {
+  //   const state = gatsbyState.getState()
+
+  //   const getMapKeyByValue = (val) => {
+  //     /**
+  //      * this is expensive because we're looking up a map key (node id) by wether our value exists within it's value which is an array of page paths
+  //      */
+  //     const returnedEntries = [
+  //       ...state.componentDataDependencies.nodes,
+  //     ].find(([, value]) => [...value].includes(val))
+
+  //     if (returnedEntries && returnedEntries.length) {
+  //       return returnedEntries[0]
+  //     }
+
+  //     return null
+  //   }
+
+  //   const nodeId = getMapKeyByValue(page.path)
+  //   // console.log(`state node id ${nodeId}`)
+  //   nodeThatCreatedThisPage = getNode(nodeId)
+  //   if (inPreviewMode()) {
+  //     debugger
+  //   }
+  // }
 
   if (nodeThatCreatedThisPage) {
     store.dispatch.previewStore.saveNodePageState({
