b91a7aebf11e9778fe6a7b4cb43409948053ddc3
Tyler Barnes <tylerdbarnes@gmail.com>
2020-11-02 14:06:30 -0800

    move gatsby-node into src

---
+++ plugin/src/gatsby-node.js
@@ -0,0 +1,33 @@
+import { runApisInSteps } from "./utils/run-steps"
+import * as steps from "./steps"
+
+module.exports = runApisInSteps({
+  onPreInit: [steps.setErrorMap],
+
+  createSchemaCustomization: [
+    steps.setGatsbyApiToState,
+    steps.ensurePluginRequirementsAreMet,
+    steps.ingestRemoteSchema,
+    [steps.createSchemaCustomization, steps.addPreviewStatusField],
+  ],
+
+  createResolvers: [steps.addPreviewStatusResolver],
+
+  sourceNodes: [
+    steps.setGatsbyApiToState,
+    [steps.persistPreviouslyCachedImages, steps.sourceNodes],
+    steps.setImageNodeIdCache,
+  ],
+
+  onPostBuild: [steps.setImageNodeIdCache],
+
+  onCreatePage: [
+    steps.savePreviewNodeIdToPageDependency,
+    steps.onCreatePageRespondToPreviewStatusQuery,
+  ],
+
+  onCreateDevServer: [
+    steps.setImageNodeIdCache,
+    steps.startPollingForContentUpdates,
+  ],
+})
