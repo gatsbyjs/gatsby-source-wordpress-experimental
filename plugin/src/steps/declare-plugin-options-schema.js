f5e8022daf3ef9b5b4bb052ba231aece2bd763a0
Tyler Barnes <tylerdbarnes@gmail.com>
2020-11-12 17:32:30 -0800

    allow null and false in many cases to reflect real-world usage

--- plugin/src/steps/declare-plugin-options-schema.js
+++ plugin/src/steps/declare-plugin-options-schema.js
@@ -1,22 +1,32 @@
 export function pluginOptionsSchema({ Joi }) {
   const getTypeOptions = () =>
     Joi.object({
-      exclude: Joi.boolean().description(
-        `Completely excludes a type from node sourcing and from the ingested schema.`
-      ),
+      exclude: Joi.boolean()
+        .allow(null)
+        .description(
+          `Completely excludes a type from node sourcing and from the ingested schema.`
+        ),
       limit: Joi.number()
         .integer()
+        .allow(null)
+        .allow(false)
         .description(
           `The maximum amount of objects of this type to fetch from WordPress.`
         ),
       excludeFieldNames: Joi.array()
         .items(Joi.string())
+        .allow(null)
+        .allow(false)
         .description(`Excludes fields on a type by field name.`),
-      nodeInterface: Joi.boolean().description(
-        `Determines wether or not this type will be treated as an interface comprised entirely of other Gatsby node types.`
-      ),
+      nodeInterface: Joi.boolean()
+        .allow(null)
+        .allow(false)
+        .description(
+          `Determines wether or not this type will be treated as an interface comprised entirely of other Gatsby node types.`
+        ),
       beforeChangeNode: Joi.function()
-        .arity(1)
+        .allow(null)
+        .allow(false)
         .description(
           `A function which is invoked before a node is created, updated, or deleted. This is a hook in point to modify the node or perform side-effects related to it.`
         ),
@@ -30,6 +40,21 @@ export function pluginOptionsSchema({ Joi }) {
       .default(true)
       .description(`Wether there will be verbose output in the terminal`),
     debug: Joi.object({
+      timeBuildSteps: Joi.boolean()
+        .default(false)
+        .description(
+          `When set to true, this option will display how long each internal step took during the build process.`
+        ),
+      disableCompatibilityCheck: Joi.boolean()
+        .default(false)
+        .description(
+          `This option disables the compatibility API check against the remote WPGraphQL and WPGatsby plugin versions.`
+        ),
+      throwRefetchErrors: Joi.boolean()
+        .default(false)
+        .description(
+          `When this is set to true, errors thrown while updating data in gatsby develop will fail the build process instead of automatically attempting to recover.`
+        ),
       graphql: Joi.object({
         showQueryVarsOnError: Joi.boolean()
           .default(false)
@@ -48,6 +73,12 @@ export function pluginOptionsSchema({ Joi }) {
           .description(
             `Determines wether or not to log non-critical errors. A non-critical error is any error which is returned alongside valid data. In previous versions of WPGraphQL this was very noisy because trying to access an entity that was private returned errors. Default is true.`
           ),
+        copyNodeSourcingQueryAndExit: Joi.string()
+          .allow(false)
+          .default(false)
+          .description(
+            `When a type name from the remote schema is entered here, the node sourcing query will be copied to the clipboard, and the process will exit.`
+          ),
         writeQueriesToDisk: Joi.boolean()
           .default(false)
           .description(
@@ -55,6 +86,13 @@ export function pluginOptionsSchema({ Joi }) {
           ),
       }),
     }).description(`Options related to debugging.`),
+    production: Joi.object({
+      hardCacheMediaFiles: Joi.boolean()
+        .default(false)
+        .description(
+          `This option is experimental. When set to true, media files will be hard-cached outside the Gatsby cache at ./.wordpress-cache/path/to/media/file.jpeg. This is useful for preventing media files from being re-downloaded when the Gatsby cache automatically clears. When using this option, be sure to gitignore the wordpress-cache directory in the root of your project.`
+        ),
+    }),
     develop: Joi.object({
       nodeUpdateInterval: Joi.number()
         .integer()
@@ -76,9 +114,11 @@ export function pluginOptionsSchema({ Joi }) {
     auth: Joi.object({
       htaccess: Joi.object({
         username: Joi.string()
+          .allow(null)
           .default(null)
           .description(`The username for your .htpassword protected site.`),
         password: Joi.string()
+          .allow(null)
           .default(null)
           .description(`The password for your .htpassword protected site.`),
       }).description(`Options related to htaccess authentication.`),
@@ -87,6 +127,7 @@ export function pluginOptionsSchema({ Joi }) {
       queryDepth: Joi.number()
         .integer()
         .positive()
+        .default(15)
         .description(
           `The maximum field depth the remote schema will be queried to.`
         ),
@@ -119,23 +160,27 @@ export function pluginOptionsSchema({ Joi }) {
     ),
     excludeFieldNames: Joi.array()
       .items(Joi.string())
+      .allow(null)
       .description(
         `A list of field names to globally exclude from the ingested schema.`
       ),
     html: Joi.object({
       useGatsbyImage: Joi.boolean()
         .default(true)
+        .allow(null)
         .description(
           `Causes the source plugin to find/replace images in html with Gatsby images.`
         ),
       imageMaxWidth: Joi.number()
         .integer()
+        .allow(null)
         .default(null)
         .description(
           `Adds a limit to the max width an image can be. If the image size selected in WP is smaller or the image file width is smaller than this those values will be used instead.`
         ),
       fallbackImageMaxWidth: Joi.number()
         .integer()
+        .allow(null)
         .default(100)
         .description(
           `If a max width can't be inferred from html this value will be passed to Sharp. If the image is smaller than this, the image file's width will be used instead.`
@@ -143,11 +188,13 @@ export function pluginOptionsSchema({ Joi }) {
       imageQuality: Joi.number()
         .integer()
         .default(90)
+        .allow(null)
         .description(
           `Determines the image quality that Sharp will use when generating inline html image thumbnails.`
         ),
       createStaticFiles: Joi.boolean()
         .default(true)
+        .allow(null)
         .description(
           `When this is true, any url's which are wrapped in "", '', or () and which contain /wp-content/uploads will be transformed into static files and the url's will be rewritten. This adds support for video, audio, and anchor tags which point at WP media item uploads as well as inline-html css like background-image: url().`
         ),
@@ -159,6 +206,7 @@ export function pluginOptionsSchema({ Joi }) {
       RootQuery: getTypeOptions().append({
         excludeFieldNames: Joi.array()
           .items(Joi.string())
+          .allow(null)
           .default([`viewer`, `node`, `schemaMd5`])
           .description(`Excludes fields on a type by field name.`),
       }),
