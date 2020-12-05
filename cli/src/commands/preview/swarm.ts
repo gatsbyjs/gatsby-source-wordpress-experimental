98a4fafd64a31277c2ad965e523868701f93fb9b
Tyler Barnes <tylerdbarnes@gmail.com>
2020-12-04 21:22:42 -0800

    add quick cli visualizatoin

--- cli/src/commands/preview/swarm.ts
+++ cli/src/commands/preview/swarm.ts
@@ -1,4 +1,6 @@
 import { GluegunCommand, GluegunToolbox } from "gluegun"
+import * as draftLog from "draftlog"
+draftLog(console)
 
 import * as fs from "fs-extra"
 import * as puppeteer from "puppeteer"
@@ -59,19 +61,81 @@ const command: GluegunCommand = {
 
     const users = await fs.readJson(wpUsersJsonPath)
 
+    const maxPreviewsEach = 10
+    const total = users.length * maxPreviewsEach
+
+    let successes = 0
+    let failures = 0
+
+    const successTimes = [0]
+    const failureTimes = [0]
+
+    function getAverageFromListOfTimes(times: number[]): number {
+      const sum = times.reduce((a, b) => a + b, 0)
+      const average = sum / times.length || 0
+
+      return Math.ceil(average) / 1000
+    }
+
+    // Input progess goes from 0 to 100
+    function progressBar(progress: number): string {
+      // Make it 50 characters length
+      let units = Math.round(progress / 2)
+
+      if (units > 50) {
+        units = 50
+      }
+
+      return (
+        `[` +
+        `=`.repeat(units) +
+        ` `.repeat(50 - units) +
+        `] ` +
+        Math.ceil(progress) +
+        `%`
+      )
+    }
+
+    const getDraft = (): string =>
+      `
+    Preview Swarming ${wpUrl}
+    with ${users.length} users making ${maxPreviewsEach} previews each
+    ---
+    %SUCCESS_NUMBER% Successes (${getAverageFromListOfTimes(
+      successTimes
+    )}s average duration)
+    %FAILURE_NUMBER% Failures (${getAverageFromListOfTimes(
+      failureTimes
+    )}s average duration)
+    %PROGRESS% of %TOTAL% Total
+    ${progressBar(((failures + successes) / total) * 100)}
+    `
+        .replace(/%SUCCESS_NUMBER%/gm, String(successes))
+        .replace(/%FAILURE_NUMBER%/gm, String(failures))
+        .replace(/%PROGRESS%/gm, String(failures + successes))
+        .replace(/%TOTAL%/gm, String(total))
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const consoleAny: any = console
+    const draft = consoleAny.draft(getDraft())
+
+    const updateDraft = (): void => draft(getDraft())
+
     await Promise.all(
       users.map(async ({ username, password }, index) => {
         const position = {
           x: [0, 1].includes(index) ? 0 : 600,
           y: [0, 2].includes(index) ? 0 : 600,
         }
+
         const browser = await puppeteer.launch({
           headless: false,
-          slowMo: 25,
+          // slowMo: 10,
           defaultViewport: null,
           args: [
             `--window-size=500,500`,
             `--window-position=${position.x},${position.y}`,
+            `--disable-features=site-per-process`,
           ],
         })
 
@@ -87,7 +151,7 @@ const command: GluegunCommand = {
 
         let counter = 0
 
-        while (counter <= 10) {
+        while (counter < maxPreviewsEach) {
           counter++
 
           await createNewPost({
@@ -100,22 +164,47 @@ const command: GluegunCommand = {
             page,
           })
 
+          const previewStart = Date.now()
+
+          let failed = false
+
           try {
             const { success } = await previewCurrentPost({
               title: `swarm user ${username} post updated at ${Date.now()}`,
               page,
               browser,
-              index,
             })
 
-            print.info(success)
+            if (success) {
+              successes++
+            } else {
+              failures++
+              failed = true
+            }
+
+            // print.info(success)
           } catch (e) {
-            print.error(e.message)
+            failed = true
+            failures++
+            // print.error(e.message)
+          } finally {
+            const previewEnd = Date.now()
+            const previewSeconds = previewEnd - previewStart
+
+            if (failed) {
+              failureTimes.push(previewSeconds)
+            } else {
+              successTimes.push(previewSeconds)
+            }
+
+            updateDraft()
           }
         }
         await browser.close()
       })
     )
+
+    updateDraft()
   },
 }
 
--- cli/src/utils/wordpress-puppeteer/login-user.ts
+++ cli/src/utils/wordpress-puppeteer/login-user.ts
@@ -21,12 +21,19 @@ export async function loginUser(input: {
     await page.goto(createURL({ path: `wp-login.php`, baseUrl }))
   }
 
+  await new Promise(resolve => setTimeout(resolve, 100))
+
   await page.focus(`#user_login`)
+  await new Promise(resolve => setTimeout(resolve, 1))
   await pressKeyWithModifier({ modifier: `primary`, key: `a`, page })
   await page.type(`#user_login`, username)
+  await new Promise(resolve => setTimeout(resolve, 100))
   await page.focus(`#user_pass`)
+  await new Promise(resolve => setTimeout(resolve, 1))
   await pressKeyWithModifier({ modifier: `primary`, key: `a`, page })
+  await new Promise(resolve => setTimeout(resolve, 100))
   await page.type(`#user_pass`, password)
 
+  await new Promise(resolve => setTimeout(resolve, 1000))
   await Promise.all([page.waitForNavigation(), page.click(`#wp-submit`)])
 }
