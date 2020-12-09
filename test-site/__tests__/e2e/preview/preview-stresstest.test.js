797daf81a56e554e2b57da562ce07310c63cfeb6
Tyler Barnes <tylerdbarnes@gmail.com>
2020-12-08 17:33:42 -0800

    add docker-host to hopefully end the madness 😱

--- test-site/__tests__/e2e/preview/preview-stresstest.test.js
+++ test-site/__tests__/e2e/preview/preview-stresstest.test.js
@@ -11,8 +11,8 @@ test(`A Preview swarm returns 0 failures when testing with 1 user making 1 previ
     users,
     previewTimeout: 300000,
     wpUrl: `http://localhost:8001`,
-    gatsbyPreviewFrontendUrl: `http://host.docker.internal:8000,http://localhost:8000`,
-    gatsbyPreviewRefreshEndpoint: `http://host.docker.internal:8000/__refresh`,
+    gatsbyPreviewFrontendUrl: `http://dockerhost:8000,http://localhost:8000`,
+    gatsbyPreviewRefreshEndpoint: `http://dockerhost:8000/__refresh`,
     debugMode: true,
   }
 
