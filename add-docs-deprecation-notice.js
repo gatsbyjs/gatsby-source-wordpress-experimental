e7caf91c24715adcefa90dd5d31dfd19a99f2cc1
Tyler Barnes <tylerdbarnes@gmail.com>
2021-02-05 13:10:52 -0800

    add deprecation notice

---
+++ add-docs-deprecation-notice.js
@@ -0,0 +1,20 @@
+const deprecationNotice = require(`./plugin/deprecation-notice.json`)
+const fs = require(`fs-extra`)
+const path = require(`path`)
+const globby = require(`globby`)
+
+;(async () => {
+  const docsPaths = await globby(path.join(__dirname, `docs`, `**`, `*.md`))
+
+  for (const mdPath of docsPaths) {
+    const fileContents = await fs.readFile(mdPath, `utf8`)
+    const relativePath = mdPath.replace(__dirname, ``)
+    const hasMovedToMessage = `This page has moved to the [Gatsby monorepo!](https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-source-wordpress${relativePath})`
+
+    const newFileContents = `**${hasMovedToMessage}**\n\n${deprecationNotice}\n\n${fileContents}`
+
+    console.log(relativePath)
+    await fs.writeFile(mdPath, newFileContents, { encoding: `utf8` })
+    process.exit()
+  }
+})()
